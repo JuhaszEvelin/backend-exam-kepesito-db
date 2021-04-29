ff260c5e992bb5302e9a316229760150e07623a1
Juhasz Evelin <juhaaszevelin@gmail.com>
2021-04-29 11:34:01 +0200

    all tests passed

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,9 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -12,4 +16,26 @@ public class JurassicPark {
         this.dbPassword = dbPassword;
     }
 
+    public List<String> checkOverpopulation() {
+        String query = "SELECT breed, expected, actual " +
+                "FROM dinosaur " +
+                "WHERE actual > expected " +
+                "ORDER BY breed ASC";
+        String column = "breed";
+        return getResult(query, column);
+    }
+
+    private List<String> getResult(String query, String column) {
+        List<String> result = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                result.add(rs.getString(column));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
